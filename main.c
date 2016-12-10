18cc7899ef06ce000a5d8610e4ea0b32e9ee0d91
MNikdan <nikdanmahdi@gmail.com>
2016-12-10 17:09:05 +0330

    isPrime

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 ﻿#include <stdio.h>
 
 int isPrime(int num) {
-
+    int d;
+    for (d = 2; d * d <= num; d++){
+        if (num % d == 0)
+            return 0;
+    }
+    return 1;
 }
 
 int absVal(int num) {
@@ -10,5 +15,5 @@ int absVal(int num) {
 
 int main() {
 	printf("Hello world!");
-	return 0;	
+	return 0;
 }؛
